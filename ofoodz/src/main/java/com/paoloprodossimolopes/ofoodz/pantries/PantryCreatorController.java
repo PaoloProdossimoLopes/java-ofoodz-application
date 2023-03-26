bb48f37b2be9db175bc606d85bc54e2e6ad3a8ad
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-03-26 20:57:30 -0300

    feat: injetando o repository para a PantryCreatorController

---
+++ ofoodz/src/main/java/com/paoloprodossimolopes/ofoodz/pantries/PantryCreatorController.java
@@ -0,0 +1,9 @@
+package com.paoloprodossimolopes.ofoodz.pantries;
+
+public class PantryCreatorController {
+    private final CreatorRepository repository;
+
+    public PantryCreatorController(CreatorRepository repository) {
+        this.repository = repository;
+    }
+}
