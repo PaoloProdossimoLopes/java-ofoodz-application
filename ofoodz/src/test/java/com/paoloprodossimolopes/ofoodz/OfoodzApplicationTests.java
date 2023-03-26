cd76c6e225065e9dd98ad6aca9f82d28204edaa8
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-03-26 06:58:59 -0300

    chore: configurando projeto de backend em spring boot

---
+++ ofoodz/src/test/java/com/paoloprodossimolopes/ofoodz/OfoodzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paoloprodossimolopes.ofoodz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfoodzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
