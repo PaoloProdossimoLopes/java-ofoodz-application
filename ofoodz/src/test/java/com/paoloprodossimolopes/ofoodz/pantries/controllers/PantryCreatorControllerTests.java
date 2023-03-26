cf930e81b1edf5253afe729227b95ba943e5dc9e
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-03-26 20:58:06 -0300

    test: ensure on initialize the controller no call repository

--- ofoodz/src/test/java/com/paoloprodossimolopes/ofoodz/pantries/controllers/PantryCreatorControllerTests.java
+++ ofoodz/src/test/java/com/paoloprodossimolopes/ofoodz/pantries/controllers/PantryCreatorControllerTests.java
@@ -1,4 +1,25 @@
 package com.paoloprodossimolopes.ofoodz.pantries.controllers;
 
+import com.paoloprodossimolopes.ofoodz.pantries.CreatorRepository;
+import com.paoloprodossimolopes.ofoodz.pantries.PantryCreatorController;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class PantryCreatorControllerTests {
+    @Test
+    void test_onInitialize_noCallsRepository() {
+        final CreatorRepositorySpy repository = new CreatorRepositorySpy();
+
+        new PantryCreatorController(repository);
+
+        Assertions.assertEquals(repository.getCreatCallCount(), 0);
+    }
+
+    private class CreatorRepositorySpy implements CreatorRepository {
+        private int createCallCount = 0;
+
+        int getCreatCallCount() {
+            return createCallCount;
+        }
+    }
 }
