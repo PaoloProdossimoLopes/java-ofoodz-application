042d41ef72854d7691f60634ecce7ad58d44f526
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-03-27 11:16:58 -0300

    feat: chamando validator de sessao para criar uma despensa

--- ofoodz/src/main/java/com/paoloprodossimolopes/ofoodz/pantries/PantryCreatorController.java
+++ ofoodz/src/main/java/com/paoloprodossimolopes/ofoodz/pantries/PantryCreatorController.java
@@ -4,12 +4,16 @@ import java.util.Optional;
 
 public class PantryCreatorController {
     private final PantryCreatorRepository repository;
+    private final SessionValidator sessionValidator;
 
-    public PantryCreatorController(PantryCreatorRepository repository) {
+    public PantryCreatorController(PantryCreatorRepository repository, SessionValidator sessionValidator) {
         this.repository = repository;
+        this.sessionValidator = sessionValidator;
     }
 
     public void create(PantryRequest request) {
+        sessionValidator.validateSessionToken(request.getValidationToken());
+        sessionValidator.validateUserIdentifier(request.getUserIdentifier());
         final Pantry pantry = new Pantry(Optional.empty(), request.getTitle());
         repository.save(pantry);
     }
